de3ffe227799473e930d4a0bbe5dd2a3cde397f6
Fernando Aguilar <fernaando@yahoo.com>
2026-03-19 20:46:35 -0600

    Se agrega archivo calculo1

---
+++ calculo1.py
@@ -0,0 +1,12 @@
+b = 30
+c = 30 * 10 * 5
+
+print('resultado', c)
+
+## cambiado el factor 102, según requerimiento del negocio
+## cambiado factor 110, según junta de control
+
+
+d = c * 110
+print('resultado total=', d)
+
